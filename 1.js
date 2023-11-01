c9d6fc7335f881b75d133e6f23aae2f272ce3f6d
Avez <avezsd77@gmail.com>
2023-11-01 14:16:39 +0530

    added 1.js file

---
+++ 1.js
@@ -0,0 +1,21 @@
+let a="avez";
+let b=3;
+console.log(a + b);
+
+const c={
+    name:'avez',
+    age:23,
+};
+c["height"]=48;// add new key to const obj
+c['name']="sayyed";// changing the value of key in const
+console.log(c);
+
+const dict={
+    'high school' :"a secondary school.",
+    yakka: "hard work",
+}
+console.log(dict);
+
+let com1="4";
+let com2=4;
+console.log(com1===com2);
